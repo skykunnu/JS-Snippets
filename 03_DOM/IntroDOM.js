e87b4ce265060a9b28c129a715b5f0a5cd2a032b
Shikhar Khandelwal <skykunnu@gmail.com>
2024-07-13 12:52:04 +0530

    init

--- 03_DOM/IntroDOM.js
+++ 03_DOM/IntroDOM.js
@@ -1,3 +1,8 @@
 // Selecting ELements in JS using DOM 
 
+// const allImages=document.images -> other way to retrieve images.  
+// const allImages=document.getElementsByTagName('img') // all the img tag will be stored inside allImages variable in the form of an array-type HTML Collection. 
+// const cssImage=document.getElementsByClassName('css-image') // all the html elements with class= css-image will be stored inside cssImage variable in the form of an array-type HTML Collection. 
+// const cssImage=document.getElementById('css-image') // uniquely select html elements.
 
+const cssImage=document.querySelector('.css-image') // use it like css selector. class-(.), id-(#), It returns the very first element with specified id & class. 
