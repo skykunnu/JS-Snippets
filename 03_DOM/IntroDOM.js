69aa858d3a54c5419be5664ed5ec2cdc14046889
Shikhar Khandelwal <skykunnu@gmail.com>
2024-07-13 15:13:14 +0530

    new

--- 03_DOM/IntroDOM.js
+++ 03_DOM/IntroDOM.js
@@ -5,4 +5,6 @@
 // const cssImage=document.getElementsByClassName('css-image') // all the html elements with class= css-image will be stored inside cssImage variable in the form of an array-type HTML Collection. 
 // const cssImage=document.getElementById('css-image') // uniquely select html elements.
 
-const cssImage=document.querySelector('.css-image') // use it like css selector. class-(.), id-(#), It returns the very first element with specified id & class. 
+// const cssImage=document.querySelector('.css-image') // it is used like a css selector. class-(.), id-(#), It returns the very first element with specified id & class. 
+
+// const cssImage=document.querySelectorAll('.css-image') // it will return all those elements who are having class-> css-image in node list form. 
