27c438d1c748d649e1a45d3a72fcf570e6763e38
Shikhar Khandelwal <skykunnu@gmail.com>
2024-07-18 09:38:47 +0530

    new

--- 03_DOM/IntroDOM.js
+++ 03_DOM/IntroDOM.js
@@ -8,3 +8,29 @@
 // const cssImage=document.querySelector('.css-image') // it is used like a css selector. class-(.), id-(#), It returns the very first element with specified id & class. 
 
 // const cssImage=document.querySelectorAll('.css-image') // it will return all those elements who are having class-> css-image in node list form. 
+
+
+// const allImages=document.querySelectorAll('img')
+
+// const imagesURL=['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzGEWd1Mp_YzMyG9xEB5qtf3wy_Cnrn1Q-Mg&s',
+//     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr_-pQnNM-qus4jw9-h8YN6H7cf0wTGiTj7Q&s',
+//     'https://images.pexels.com/photos/2166553/pexels-photo-2166553.jpeg?cs=srgb&dl=pexels-freestockpro-2166553.jpg&fm=jpg'
+// ]
+
+// allImages[0].src=imagesURL[0]
+// allImages[1].src=imagesURL[1]
+// allImages[2].src=imagesURL[2]
+
+// for(let i=0;i<allImages.length;i++){
+//     allImages[i].src=imagesURL[i]
+// }
+
+// allImages.forEach((image,i) => {
+//     image.src=imagesURL[i]
+// });
+
+// const ul=document.querySelector('ul')
+
+// const imageInsideUl=ul.querySelector('.css-image') // we can use any html element with query selector,all,className,tagName but not available with ID. 
+
+
