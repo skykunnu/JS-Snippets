28b67ba2cdfdcc8b0b9ec51d0821483659936972
Shikhar Khandelwal <skykunnu@gmail.com>
2024-07-18 16:47:15 +0530

    new

---
+++ 03_DOM/DOM Traversing/parents,sibling,child.js
@@ -0,0 +1,7 @@
+// Traversing Parent, Sibling & Child elements using JS
+
+let innerElement=document.getElementById('inner').parentNode;
+
+console.log(innerElement) // outer div will be returned by inner div.  
+
+
