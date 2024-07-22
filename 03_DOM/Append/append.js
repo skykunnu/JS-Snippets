534b9da11a21c5d1dd93f104ba19eddf61d41564
Shikhar Khandelwal <skykunnu@gmail.com>
2024-07-22 16:39:59 +0530

    new

---
+++ 03_DOM/Append/append.js
@@ -0,0 +1,13 @@
+
+const h1=document.querySelector('h1')
+const container=document.querySelector('.container')
+const card=document.querySelector('.card')
+
+// container.appendChild(h1) // It will remove h1 from top and place it in the end of container.
+// container.appendChild(h1.cloneNode()) <h1> </h1> // It will not remove h1 from the top instead it will make a copy of it and put it in the end of container block. 
+// if we want text content should also appear, then pass truthy value as an arguments to it. 
+// container.appendChild(h1.cloneNode(1)) <h1>appendChild & append</h1>
+
+
+
+
