b39749836dfc50a9909144573ea9a77710db28b9
Shikhar Khandelwal <skykunnu@gmail.com>
2024-09-04 17:35:26 +0530

    new

--- 01_basic/05_DataTypesSummary.js
+++ 01_basic/05_DataTypesSummary.js
@@ -35,7 +35,7 @@ const myFunction = function () {
 
 //Whenever we define variables with primitive datatypes, We get the copy of the variable in a stack, whatever changes in the original variable will reflect only in the original variable. not in the copied variable.
 
-//Whenever we define variables with Non-primitive(Reference) datatypes, We get the (Reference)Original-value of the variable in Heap, whatever changes in the original variable will reflects in the original as well as copied variable also.
+//Whenever we define variables with Non-primitive(Reference) datatypes, We get the (Reference)Original-value of the variable in Heap, whatever changes made in the original variable will reflect in the original as well as copied variable also.
 
 let mySchoolname = "Shikhar";
 let anotherName = mySchoolname;
